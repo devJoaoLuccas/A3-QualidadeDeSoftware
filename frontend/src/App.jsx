d562676298271f25adea39545a97429c979aaeda
João Luccas <101368682+devJoaoLuccas@users.noreply.github.com>
2024-04-13 21:09:18 -0300

    build(react-toastify): add react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import 'react-toastify/dist/ReactToastify.css';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './router/Routes';
+import { ToastContainer } from 'react-toastify'
+
 
 function App() {
 
 
   return (
-        <RouterProvider router={routes} />
+        <>
+          <RouterProvider router={routes} />
+          <ToastContainer />
+        </>
   )
 }
 
--- frontend/src/components/CalcFunctions/CalcularImc.jsx
+++ frontend/src/components/CalcFunctions/CalcularImc.jsx
@@ -1,16 +1,18 @@
+import { toast } from "react-toastify";
+
 export const CalcularImc = (peso, altura) => {
     if (altura <= 0) {
-        alert('A altura não pode ser igual a 0');
-        throw new Error('A altura não pode ser igual 0')
+        toast.error('A altura não pode ser igual a 0');
+        throw new Error('A altura não pode ser igual 0');
     } else if (altura >= 2.2) {
-        alert('Altura não válida');  
+        toast.error('Altura não é válida');
         throw new Error('A altura não é valida');
     } else if (peso <= 0) {
-        alert('O peso não pode ser igual 0');
-        throw new Error('O peso não pode ser igual a 0')
+        toast.error('O peso não pode ser igual a 0');
+        throw new Error('O peso não pode ser igual a 0');
     } else if (peso >= 250) {
-        alert('O peso não é válido');
-        throw new Error('O peso não é valido')
+        toast.error('O peso não é valido');
+        throw new Error('O peso não é valido');
     }  else {
         return peso / (altura * altura);
     }
